ae476e54f2a379bfc21a0d41349511869da5b39b
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2022-07-11 19:29:56 +0530

    commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("name").innerHTML="Yash Agrawal";
+document.getElementById("roll-no").innerHTML="201500810";
+document.getElementById("university").innerHTML="GLA University, Mathura";
+var date=new Date();
+document.getElementById("date").innerHTML=date.toDateString();
